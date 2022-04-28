ae8fcc3cfd0a00de458bb1d34171ea674be62b54
RamanSaksonau <kramans1990@outlook.com>
2022-04-29 01:36:04 +0300

    add:transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if(!Array.isArray(arr)){
+      throw new Error ('\'arr\' parameter must be an instance of the Array!');
+  } 
+//   '--discard-next'
+//  '--discard-prev'
+//  '--double-next' 
+//  '--double-prev'
+let result = new Array();
+
+arr.forEach(item=>result.push(item));
+
+var T = result.length;
+    for(var i = 0 ; i<T;i++){
+           if(result[i] == '--discard-next'){
+               if (i<  result.length -1)
+               {  
+                result.splice(i+1,1);                   
+                }  
+                result[i]= result[i] + "#";
+                   i = 0;
+                   T = result.length;
+           }
+           /////
+           if(result[i] == '--discard-prev'){
+            if (i>0)
+            {  
+                result.splice(i-1,1);  
+                result[i-1] = result[i-1] + "#";                 
+             }  
+             if(i == 0){  result[i] = result[i] + "#";}
+           
+                i = 0;
+                T = result.length;
+        }
+       
+        /////
+        if(result[i] == '--double-next'){
+            if (i<  result.length -1)
+            {  
+                result.splice(i+1,0,result[i+1]); 
+                                 
+             }  
+             if(i == result.length-1 ){
+                  
+                }
+              result[i] = result[i]+"#";
+                i = 0;
+                T = result.length;
+        }
+        /////
+         /////
+         if(result[i] == '--double-prev'){
+            if (i>0)
+            {  
+                result.splice(i-1,0,result[i-1]);  
+                result[i+1] = result[i+1]+"#"         
+             }  
+            if(i == 0 ){ result[i] = result[i]+"#";}
+            
+                i = 0;
+                T = result.length;
+        }
+          
+          
+      
+    }
+    result = result.filter(item=> item!=("--discard-next#"));
+    result = result.filter(item=> item!=("--discard-prev#"));
+    result = result.filter(item=> item!=("--double-next#"));
+    result = result.filter(item=> item!=("--double-prev#"));
+    console.log(arr);
+    return result;
 }
 
 module.exports = {
   transform
 };
+
