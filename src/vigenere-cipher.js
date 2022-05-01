3c54af23866dbc624e738df64c39b93305285d73
RamanSaksonau <kramans1990@outlook.com>
2022-05-01 16:06:53 +0300

    add : viginer-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,96 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+
+  isDirect = true
+  
+   N = 26
+  constructor(a){
+
+      this.isDirect = a == false? false:true; 
+  }    
+
+      encrypt(message,key) {
+      if(arguments.length!=2)
+      {
+          throw new Error ('Incorrect arguments!');
+      }
+      if(message  == undefined){
+        throw new Error ('Incorrect arguments!');
+    }
+    if(key  == undefined){
+        throw new Error ('Incorrect arguments!');
+    }
+      message = message.toString().toLowerCase();
+      key = key.toString().toLowerCase();
+    let  ABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+   let keyWordIndex = 0;
+   let keyWordLength = key.toString().length;
+      let messageArr = message.split('');      
+      let result = "";
+      messageArr.forEach(element => {
+          let i =  (ABC.indexOf(element));
+          let b = ABC.indexOf(key[keyWordIndex]);
+          if(i>=0){
+            let index =  (ABC.indexOf(element) + ABC.indexOf(key[keyWordIndex])) %26;
+            result += ABC[index];
+            keyWordIndex++;
+            if(keyWordIndex == keyWordLength){keyWordIndex = 0;}}
+            else{result += element;}
+      });//AEIHQX SX DLLU!  
+      
+      if(this.isDirect){
+      return result.toUpperCase();
+      }
+      else{
+          let arr = result.split('').reverse();
+          let x = arr.reduce((a,b) =>'' + a+ ''+ b , '');
+          return x.toUpperCase();
+          }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  decrypt(message,key) {
+      if(arguments.length!=2)
+      {
+          throw new Error ('Incorrect arguments!');
+      }
+      if(message  == undefined){
+        throw new Error ('Incorrect arguments!');
+    }
+    if(key  == undefined){
+        throw new Error ('Incorrect arguments!');
+    }
+    message = message.toString().toLowerCase();
+    key = key.toString().toLowerCase();
+    let  ABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ 
+ 
+    let keyWordIndex = 0;
+   let keyWordLength = key.toString().length;
+      let messageArr = message.split('');      
+      let result = "";
+
+      messageArr.forEach(element => {
+         
+          let i =  (ABC.indexOf(element));
+          if(i>=0){
+            let index =  (ABC.indexOf(element) +  26 - ABC.indexOf(key[keyWordIndex])) %26;
+            result += ABC[index];
+            keyWordIndex++;
+            if(keyWordIndex == keyWordLength){keyWordIndex = 0;}}
+            else{result += element;}
+      });//AEIHQX SX DLLU!  
+      
+      if(this.isDirect){
+      return result.toUpperCase();
+      }
+      else{
+          let arr = result.split('').reverse();
+          let x = arr.reduce((a,b) =>'' + a+ ''+ b , '');
+          return x.toUpperCase();
+          }
   }
 }
 
